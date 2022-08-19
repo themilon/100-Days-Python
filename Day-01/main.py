01dc0df9c12a3e34a51cabe1ebe59485078d6ec5
Moniruzzaman <themilon9@gmail.com>
2022-08-19 16:38:21 +0600

    Brand Name Generator

--- Day-01/main.py
+++ Day-01/main.py
@@ -1,1 +1,6 @@
-print("Day 01 Project- Brand Name Generator ")
+print("Welcome to the Brand Name Generator. ")
+
+city = input("Which city did you grow up in ? \n")
+pet = input("What is the name of a pet ? \n")
+
+print("Your band name could be " + city + " " + pet)
